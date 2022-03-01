c5a5683fc75c9fe5e2fbcc6f221cc8f4a29ca6b7
alinavm1 <alina.levytska@visualmagic.se>
2022-03-01 13:44:49 +0100

    Create Part3.py
    Part 3

---
+++ Part3.py
@@ -0,0 +1,47 @@
+#Function 1:    Function for an input of two variables and one return value. Making Tigrocat :)
+def firstValue (Tiger):
+    print(Tiger)
+    return Tiger
+def secondValue (Cat):
+    print (Cat)
+    return Cat
+TigerValue = 2
+CatValue = 1
+
+def AdditionFunction(A,B):
+  result = firstValue(A)+secondValue(B)
+  # 3    =      2       +   1
+  print(result)
+  return result 
+
+AdditionFunction(TigerValue,CatValue)
+
+
+
+
+
+
+
+#Function 2A:    Function for one input value and prints said value to the console. Printing Cat
+val = input (CatValue)
+print(CatValue)
+
+
+
+
+
+
+
+
+#Function 2B: With output of both text and number  
+num = input ("777")
+print(num)
+name1 = input ("Catotiger")
+print (name1)
+
+#printing
+print ("777", type (num))
+print ("Catotiger", type(name1))
+
+
+
